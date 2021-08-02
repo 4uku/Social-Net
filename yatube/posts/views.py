6a273b42dfd594f55cf0a754951cb752fd51cd1c
Дмитрий <demonekr@yandex.ru>
2021-08-02 20:01:56 +0300

    v2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -39,7 +39,6 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    # author_following = author.following.all().filter(user=request.user)
     if ((request.user != author)
        and not Follow.objects.filter(author=author,
                                      user=request.user).exists()):
